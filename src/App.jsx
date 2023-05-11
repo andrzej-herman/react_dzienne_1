5821cf1d203b6ac05decb5c9365337b4f40ba4ce
andrzej-herman <aherman@san.edu.pl>
2023-05-11 11:00:09 +0200

    Add project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <div className="green"></div>
+      <div className="red"></div>
+      <h1>Herman</h1>
+    
+    </div>
+  );
+}
+
+export default App;
